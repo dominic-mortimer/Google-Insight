faee13a5cb81c3fb83100e0f5452157a78c4d44f
Okeke-miles <franklinokeke9@gmail.com>
2021-07-08 16:19:52 -0400

    Work done

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -14,12 +14,6 @@ function MapPage() {
                     <img className="map-hero__img" src={mapGif} alt="Map"/>
                     <p className="map-hero__text">With Google Insight, you can dive deeper to learn how your location data is being used to help create a more predictive future. With the map visualizer, Google aims to educate users and provide more insight into where this data comes from.</p>
                 </section>
-
-                <section className="durationmap">
-                {/* <h2>Average trip duration by zones in New York City</h2> */}
-                {/* <p>For drivers of all kind. New York City can be a hassle to navigate through especially when travelling between neighbourhoods. Using taxi data, Google Insight is able to provide a visualization into the average trip duration within a given zone in - this allows drivers to understand what zones are congested versus which are easier to drive through.</p> */}
-                <img className="durationmap__style"src={durationmap}/>
-                </section>
             </main>
         )
 }
