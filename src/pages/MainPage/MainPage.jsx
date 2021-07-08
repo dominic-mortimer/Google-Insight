26f651ba992bcb63118903ca9a0349125dc35dc1
LaurieGami <laurievilleton@gmail.com>
2021-07-08 13:01:52 -0700

    changed icons and final details

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -12,7 +12,7 @@ function MainPage() {
                     <img className="main-hero__privacy-icon" src={privacyIcon} alt="Privacy Icon" />
                     <h2 className="main-hero__title">Providing visual insight on your mobility and how we use this data.</h2>
                     <p className="main-hero__text">With Google Insight, you can dive deeper to learn how your location data is being used to help create a more predictive future. With the map visualizer, Google aims to educate users and provide more insight into where this data comes from.</p>
-                    <Link className="main-hero__button" to="/map">See My Map</Link>
+                    <Link className="main-hero__button" to="/map">View Data Maps</Link>
                 </section>
                 <section className="explore">
                     {/* <h2 className="explore__title">Explore what we do to improve your commute</h2>
