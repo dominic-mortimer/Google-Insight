a3e69590644e45ca193e01d245a324d1d087d497
LaurieGami <laurievilleton@gmail.com>
2021-07-08 10:44:56 -0700

    added an actual png map

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -1,6 +1,7 @@
 import './MapPage.scss';
 
 import privacyIcon from '../../assets/images/privacy-icon.svg';
+import map from '../../assets/images/choropleth_map.png';
 
 function MapPage() {
     
@@ -9,7 +10,7 @@ function MapPage() {
                 <section className="map-hero">
                     <img className="map-hero__privacy-icon" src={privacyIcon} alt="Privacy Icon" />
                     <h2 className="map-hero__title">Insert Header Here</h2>
-                    <img className="map-hero__img" alt="Map"/>
+                    <img className="map-hero__img" src={map} alt="Map"/>
                     <p className="map-hero__text">With Google Insight, you can dive deeper to learn how your location data is being used to help create a more predictive future. With the map visualizer, Google aims to educate users and provide more insight into where this data comes from.</p>
                 </section>
             </main>
