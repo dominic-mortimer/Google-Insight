25f23e466ecc9b9f5d1e5937485aaeca32cd78db
LaurieGami <laurievilleton@gmail.com>
2021-07-08 08:54:15 -0700

    created the two main pages, installed react router and set up routes, added some image content

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import './MainPage.scss';
+
+function MainPage() {
+    
+        return (
+            <main className="main">
+                <h1>Hello Goole</h1>
+            </main>
+        )
+}
+
+export default MainPage;
---
+++ src/pages/MapPage/MapPage.jsx
@@ -0,0 +1,12 @@
+import './MapPage.scss';
+
+function MapPage() {
+    
+        return (
+            <main className="main">
+                <h1>This is the map page!</h1>
+            </main>
+        )
+}
+
+export default MapPage;
