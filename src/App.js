2c7570d9aac7ea97919412ee7ac9ce65d6309878
LaurieGami <laurievilleton@gmail.com>
2021-07-08 12:45:31 -0700

    fixed parts of the header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.scss';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header/Header';
-import Header2 from './components/Header2/Header2';
 import MainPage from './pages/MainPage/MainPage';
 import MapPage from './pages/MapPage/MapPage';
 
@@ -13,8 +12,6 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Header2 />
-      {/* <header>Google Navbar will be here</header> */}
       <Switch>
         <Route exact path="/" component={MainPage} />
         <Route exact path="/map" component={MapPage} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,47 @@
 import React from "react"
 import "../Header/Header.scss"
 import googleLogo from "../../assets/images/Google-logo-768x432.png"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 function Header() {
     return (
-        <div className="header__body">
-            <p className="header__title"><img className="header__logo" src={googleLogo} alt="googlelogo" /> Insights</p>
-            <div className="header__navbar">
-
-                <p className="visualise__style">Visualize My Data</p>
-                <div className="navbar__body">
-                    <Link to="/" activeclassName="overview__style">
-                        <p>Overview</p>
-                    </Link>
-                    <p>How It Works</p>
-                    <Link to="/map" activeclassName="map__style">
-                        <p>See My Map</p>
-                    </Link>
-                </div>
-                <p className="account__style">Google Account</p>
-
+        <header className="header">
+            <div className="header__logo">
+                <img className="header__logo-img" src={googleLogo} alt="googlelogo" />
+                <p className="header__logo-text">Insight</p>
             </div>
-        </div>
+            
+            <nav className="navbar">
+                <p className="navbar__main">Visualize Data</p>
+                <ul className="navbar__list">
+                    <NavLink exact to="/" activeClassName="navbar__link--active" className="navbar__link">
+                        <li className="navbar__list-item">
+                            Overview
+                        </li>
+                    </NavLink>
+                    <a href="/" className="navbar__link">
+                        <li className="navbar__list-item">
+                            How It Works
+                        </li>
+                    </a>
+                    <NavLink to="/map" activeClassName="navbar__link--active" className="navbar__link">
+                        <li className="navbar__list-item">
+                            View Data Maps
+                        </li>
+                    </NavLink>
+                    <a href="/" className="navbar__link">
+                        <li className="navbar__list-item">
+                            Usages
+                        </li>
+                    </a>
+                    <a href="/" className="navbar__link navbar__link--account">
+                        <p className="navbar__list-item">
+                            Google Account
+                        </p>
+                    </a>
+                </ul>
+            </nav>
+        </header>
     );
 }
 
